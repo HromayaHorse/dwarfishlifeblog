16e8ec3d13b2d543a492d13b8a92a8b52059bf9b
HromayaHorse <egorhejfec@gmail.com>
2021-11-05 21:14:31 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'home/links'
   # get 'home/index'
   root 'home#index'
+  get "*path" => redirect("/")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
